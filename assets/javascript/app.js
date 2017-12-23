be2696c7680844990c53d3ca5935f612c3aed769
Shawn Feiz <shawnfeiz@gmail.com>
2017-12-23 11:47:11 -0600

    Create first initial questions. Include Amazon favicon. Click the button to start timer. More to come.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,12 @@ var noGuess = 0;
 var wins = 0;
 var losses = 0;
 
+//make an array of objects or objects within objects for Q/A  
+var questions = [];
+
 //hide elements
 // $(".panel-title").hide();
 
-//make an array of objects or objects within objects for Q/A  
-var questions = [];
 
 //set a timer to count down from 10
 function start() {
@@ -25,18 +26,27 @@ function countDown(){
     if(time === 0){
         losses++;
         // gameStop();
+    } 
+    else {
+        playGame();
     }
 };
 
-start();
-
 
 //choose a question and display it to the user
+function playGame(){
+
+};
+
+
+
 
 //if user picks the right answer inrease wins++, congrats them,
 //move onto next question
 
-
+$(".startButton").on("click", function(){
+    start();
+});
 
 
 
